f3841274679de606fe597daf84671c02ae861da8
Kingdon065 <masiyuan90@163.com>
2021-01-04 19:44:47 +0800

    1.0

---
+++ Test02.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 10;
+    int b = a * a;
+    cout << a << "的平方等于" << b << endl;
+    cout << "Hello, Vs Code" << endl;
+    return 0;
+}
